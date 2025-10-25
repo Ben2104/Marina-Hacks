6b544260ac25614da51ed9a7ed2461505dd308e4
Kien <ki3n98@gmail.com>
2025-10-25 10:54:13 -0700

    add speechToText file

---
+++ back_end/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "query": q}
+
+@app.post("/predict")
+def predict(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "query": q}
+
